9326cdf6071f19fea15ba7eee374d4a6d2782176
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2020-11-14 17:42:41 +0100

    Introduce MVVM Toolkit

--- WpfPlayground/ViewModel/MainWindowViewModel.cs
+++ WpfPlayground/ViewModel/MainWindowViewModel.cs
@@ -1,19 +1,9 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using JetBrains.Annotations;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
 
 namespace WpfPlayground.ViewModel
 {
-    public sealed class MainWindowViewModel : INotifyPropertyChanged
+    public sealed class MainWindowViewModel : ObservableObject
     {
         public string Title { get; } = "Wpf Playground";
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null!)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
     }
 }
