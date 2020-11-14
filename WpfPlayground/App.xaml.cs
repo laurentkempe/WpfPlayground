0c790ae27897db93297512a8e852e9caaf6436f5
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2020-11-14 16:40:37 +0100

    Create the MainWindow on startup of App

--- WpfPlayground/App.xaml.cs
+++ WpfPlayground/App.xaml.cs
@@ -1,9 +1,21 @@
-﻿namespace WpfPlayground
+﻿using System.Threading;
+using System.Windows;
+
+namespace WpfPlayground
 {
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Thread.CurrentThread.Name = "UI";
+            
+            base.OnStartup(e);
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }
