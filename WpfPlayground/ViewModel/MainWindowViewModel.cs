c4e4e521f9fabbaea6f759614e69c12f01ba9845
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2020-11-14 18:03:35 +0100

    Add a RelayCommand to update the Title

--- WpfPlayground/ViewModel/MainWindowViewModel.cs
+++ WpfPlayground/ViewModel/MainWindowViewModel.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+﻿using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 
 namespace WpfPlayground.ViewModel
 {
     public sealed class MainWindowViewModel : ObservableObject
     {
-        public string Title { get; } = "Wpf Playground";
+        private string _title = "Wpf Playground";
+
+        public MainWindowViewModel()
+        {
+            UpdateTitleCommand = new RelayCommand<string>(UpdateTitle);
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public ICommand UpdateTitleCommand { get; }
+
+        private void UpdateTitle(string updatedTitle)
+        {
+            Title = updatedTitle;
+        }
     }
 }
