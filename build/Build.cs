00840528ae35e4c9a16fb732d16187328c1329ed
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2020-12-04 22:52:30 +0100

    Add Produces to the Restore target

--- build/Build.cs
+++ build/Build.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.TeamCity;
+using Nuke.Common.CI.TeamCity.Configuration;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
@@ -47,6 +48,7 @@ class Build : NukeBuild
         });
 
     Target Restore => _ => _
+        .Produces(RootDirectory / "WpfPlayground" / "obj" )
         .Executes(() =>
         {
             DotNetRestore(s => s
@@ -55,6 +57,7 @@ class Build : NukeBuild
 
     Target Compile => _ => _
         .DependsOn(Restore)
+        .Consumes(Restore)
         .Executes(() =>
         {
             DotNetBuild(s => s
